97269720e3746606fc65ad522a76605229bd4c33
Adam Albrecht <adam.albrecht@gmail.com>
2020-03-09 17:54:14 -0400

    Remove old notifications code
    This feature was removed at some point, but
    there was some code leftover

--- options.js
+++ options.js
@@ -4,12 +4,10 @@ document.addEventListener("DOMContentLoaded", function(){
 });
 
 var selectReqInterval;
-var radioNotifications;
 var radioBackgroundTabs;
 
 function initVariables() {
   selectReqInterval = document.getElementById("RequestInterval");
-  radioNotifications = document.getElementsByName("Notifications");
   radioBackgroundTabs = document.getElementsByName("BackgroundTabs");
 }
 
@@ -20,12 +18,6 @@ function restoreOptions() {
     if (selectReqInterval[i].value == reqInterval) {
       selectReqInterval[i].selected = "true";
       break;
-    }
-  }
-  var notifications = localStorage["HN.Notifications"];
-  for (var i=0; i<radioNotifications.length; i++) {
-    if (radioNotifications[i].value == notifications) {
-      radioNotifications[i].checked = "true";
     }
   }
   var backgroundTabs = localStorage["HN.BackgroundTabs"];
@@ -39,13 +31,6 @@ function restoreOptions() {
 function saveOptions() {
   var interval = selectReqInterval.children[selectReqInterval.selectedIndex].value;
   localStorage["HN.RequestInterval"] = interval;
-
-  for (var i=0; i<radioNotifications.length; i++) {
-    if (radioNotifications[i].checked) {
-      localStorage["HN.Notifications"] = radioNotifications[i].value;
-      break;
-    }
-  }
 
   for (var i=0; i<radioBackgroundTabs.length; i++) {
     if (radioBackgroundTabs[i].checked) {
