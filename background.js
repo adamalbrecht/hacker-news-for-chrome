665915667672c97f01b8bcaa733e77f19a4ac211
Adam Albrecht <adam.albrecht@gmail.com>
2020-03-09 17:18:44 -0400

    Convert tabs to spaces

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 var firstRequest = true;
 function startRequest() {
-	UpdateIfReady(firstRequest);
-	firstRequest = false;
-	window.setTimeout(startRequest, 60000);
+  UpdateIfReady(firstRequest);
+  firstRequest = false;
+  window.setTimeout(startRequest, 60000);
 }
 //If any options are not already set, they will be set to defaults here
 SetInitialOption("HN.RequestInterval", 1200000);
--- core.js
+++ core.js
@@ -6,26 +6,26 @@ var OnFeedFail = null;
 var retryMilliseconds = 120000;
 
 function SetInitialOption(key, value) {
-	if (localStorage[key] == null) {
-		localStorage[key] = value;
-	}
+  if (localStorage[key] == null) {
+    localStorage[key] = value;
+  }
 }
 
 function UpdateIfReady(force) {
-	var lastRefresh = parseFloat(localStorage["HN.LastRefresh"]);
-	var interval = parseFloat(localStorage["HN.RequestInterval"]);
-	var nextRefresh = lastRefresh + interval;
-	var curTime = parseFloat((new Date()).getTime());
-	var isReady = (curTime > nextRefresh);
-	var isNull = (localStorage["HN.LastRefresh"] == null);
-	if ((force == true) || (localStorage["HN.LastRefresh"] == null)) {
-		UpdateFeed();
-	}
-	else {
-	  if (isReady) {
-	    UpdateFeed();
-	  }
-	}
+  var lastRefresh = parseFloat(localStorage["HN.LastRefresh"]);
+  var interval = parseFloat(localStorage["HN.RequestInterval"]);
+  var nextRefresh = lastRefresh + interval;
+  var curTime = parseFloat((new Date()).getTime());
+  var isReady = (curTime > nextRefresh);
+  var isNull = (localStorage["HN.LastRefresh"] == null);
+  if ((force == true) || (localStorage["HN.LastRefresh"] == null)) {
+    UpdateFeed();
+  }
+  else {
+    if (isReady) {
+      UpdateFeed();
+    }
+  }
 }
 
 function UpdateFeed() {
@@ -37,13 +37,13 @@ function onRssSuccess(doc) {
     handleFeedParsingFailed("Not a valid feed.");
     return;
   }
- 	links = parseHNLinks(doc);
-	SaveLinksToLocalStorage(links);
-	if (buildPopupAfterResponse == true) {
-		buildPopup(links);
-		buildPopupAfterResponse = false;
-	}
-	localStorage["HN.LastRefresh"] = (new Date()).getTime();
+  links = parseHNLinks(doc);
+  SaveLinksToLocalStorage(links);
+  if (buildPopupAfterResponse == true) {
+    buildPopup(links);
+    buildPopupAfterResponse = false;
+  }
+  localStorage["HN.LastRefresh"] = (new Date()).getTime();
 }
 
 function updateLastRefreshTime() {
@@ -86,10 +86,10 @@ function parseXml(xml) {
 }
 
 function parseHNLinks(doc) {
-	var entries = doc.getElementsByTagName('entry');
-	if (entries.length == 0) {
-	  entries = doc.getElementsByTagName('item');
-	}
+  var entries = doc.getElementsByTagName('entry');
+  if (entries.length == 0) {
+    entries = doc.getElementsByTagName('item');
+  }
   var count = Math.min(entries.length, maxFeedItems);
   var links = new Array();
   for (var i=0; i< count; i++) {
@@ -128,29 +128,29 @@ function parseHNLinks(doc) {
 }
 
 function SaveLinksToLocalStorage(links) {
-	localStorage["HN.NumLinks"] = links.length;
-	for (var i=0; i<links.length; i++) {
-		localStorage["HN.Link" + i] = JSON.stringify(links[i]);
-	}
+  localStorage["HN.NumLinks"] = links.length;
+  for (var i=0; i<links.length; i++) {
+    localStorage["HN.Link" + i] = JSON.stringify(links[i]);
+  }
 }
 
 function RetrieveLinksFromLocalStorage() {
-	var numLinks = localStorage["HN.NumLinks"];
-	if (numLinks == null) {
-		return null;
-	}
-	else {
-		var links = new Array();
-		for (var i=0; i<numLinks; i++) {
-			links.push(JSON.parse(localStorage["HN.Link" + i]))
-		}
-		return links;
-	}
+  var numLinks = localStorage["HN.NumLinks"];
+  if (numLinks == null) {
+    return null;
+  }
+  else {
+    var links = new Array();
+    for (var i=0; i<numLinks; i++) {
+      links.push(JSON.parse(localStorage["HN.Link" + i]))
+    }
+    return links;
+  }
 }
 
 function openOptions() {
-	var optionsUrl = chrome.extension.getURL('options.html');
-	chrome.tabs.create({url: optionsUrl});
+  var optionsUrl = chrome.extension.getURL('options.html');
+  chrome.tabs.create({url: optionsUrl});
 }
 
 function openLink(e) {
@@ -159,23 +159,23 @@ function openLink(e) {
 }
 
 function openLinkFront(e) {
-	e.preventDefault();
-	openUrl(this.href, true);
+  e.preventDefault();
+  openUrl(this.href, true);
 }
 
 function printTime(d) {
-	var hour   = d.getHours();
-	var minute = d.getMinutes();
-	var ap = "AM";
-	if (hour   > 11) { ap = "PM";             }
-	if (hour   > 12) { hour = hour - 12;      }
-	if (hour   == 0) { hour = 12;             }
-	if (minute < 10) { minute = "0" + minute; }
-	var timeString = hour +
-					':' +
-					minute +
-					" " +
-					ap;
+  var hour   = d.getHours();
+  var minute = d.getMinutes();
+  var ap = "AM";
+  if (hour   > 11) { ap = "PM";             }
+  if (hour   > 12) { hour = hour - 12;      }
+  if (hour   == 0) { hour = 12;             }
+  if (minute < 10) { minute = "0" + minute; }
+  var timeString = hour +
+          ':' +
+          minute +
+          " " +
+          ap;
   return timeString;
 }
 
@@ -187,21 +187,21 @@ function openUrl(url, take_focus) {
   }
   chrome.tabs.create({url: url, selected: take_focus});
 }
-	
+  
 function hideElement(id) {
-	var e = document.getElementById(id);
-	e.style.display = 'none';
+  var e = document.getElementById(id);
+  e.style.display = 'none';
 }
 
 function showElement(id) {
-	var e = document.getElementById(id);
-	e.style.display = 'block';
+  var e = document.getElementById(id);
+  e.style.display = 'block';
 }
 
 function toggle(id) {
-	var e = document.getElementById(id);
-	if(e.style.display == 'block')
-		e.style.display = 'none';
-	else
-		e.style.display = 'block';
+  var e = document.getElementById(id);
+  if(e.style.display == 'block')
+    e.style.display = 'none';
+  else
+    e.style.display = 'block';
 }
